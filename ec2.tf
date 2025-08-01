b66a0da830a5fa51006b68b0c418abda2f5fdbb9
elocobemartin <elocobemart@gmail.com>
2025-08-01 19:21:28 +0200

    ec2 fix-2

--- ec2.tf
+++ ec2.tf
@@ -2,15 +2,14 @@ provider "aws" {
   region = "us-east-1"
 }
 # EC2 instances
-resource "aws_instance" "example" {
-  count         = 3
-  ami           = "ami-020cba7c55df1f615" 
-  instance_type = "t2.micro"
-  tags = {
-    Name = "MyEC2Instance-${count.index + 1}"
-  }
+resource "aws_instance" "web" { 
+count = 2
+ami           = "ami-0c55b159cbfafe1f0" 
+instance_type = "t2.micro" 
+tags = { Name = "web-server" } }
+  
 }
 # output the public Ips of the instance
   output "instance_public_ips" {
-    value=aws_instance.example[*].public_ip
+    value=aws_instance.web[*].public_ip
   }

388b7db731913c6c9af8a30ca311a7af24225998
elocobemartin <elocobemart@gmail.com>
2025-08-01 16:17:28 +0200

    my ec2 pipeline2

--- ec2.tf
+++ ec2.tf
@@ -2,9 +2,9 @@ provider "aws" {
   region = "us-west-1"
 }
 # EC2 instances
-resource "aws_instance" "example" {
+resource "aws_instance" "ec2 instance" {
   count         = 3
-  ami           = "ami-020cba7c55df1f615"
+  ami           = "ami-020cba7c55df1f615" 
   instance_type = "t2.micro"
   tags = {
     Name = "MyEC2Instance-${count.index + 1}"

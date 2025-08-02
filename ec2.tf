e1d1c9c1ed3ba133b3970cf4164298660d25b2bb
elocobemartin <elocobemart@gmail.com>
2025-08-02 16:41:26 +0200

    my last pipeline

--- ec2.tf
+++ ec2.tf
@@ -1,15 +1,19 @@
 provider "aws" {
   region = "us-east-1"
 }
+
 # EC2 instances
 resource "aws_instance" "web" { 
-count = 2
-ami           = "ami-0c55b159cbfafe1f0" 
-instance_type = "t2.micro" 
-tags = { Name = "web-server" } }
-  
-}
-# output the public Ips of the instance
-  output "instance_public_ips" {
-    value=aws_instance.web[*].public_ip
+  count         = 2
+  ami           = "ami-0c55b159cbfafe1f0" 
+  instance_type = "t2.micro" 
+
+  tags = { 
+    Name = "web-server-${count.index}" 
   }
+}
+
+# Output the public IPs of the instances
+output "instance_public_ips" {
+  value = aws_instance.web[*].public_ip
+}

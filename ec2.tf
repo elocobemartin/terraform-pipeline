3d3f3e543f47bad86849af90aa81991450c05fb6
elocobemartin <elocobemart@gmail.com>
2025-08-02 18:44:40 +0200

    change ec2 ami

--- ec2.tf
+++ ec2.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  region = "us-west-1"
+  region = "us-east-1"
 }
 
 # EC2 instances
 resource "aws_instance" "web" { 
   count         = 2
-  ami           = "ami-0c55b159cbfafe1f0" 
+  ami           = "ami-0fc5d935ebf8bc3bc" 
   instance_type = "t2.micro" 
 
   tags = { 
